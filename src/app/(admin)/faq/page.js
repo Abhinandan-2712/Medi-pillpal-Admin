1bda4f2b9997c803479587d22fe764e3c0600179
Abhinandan-2712 <abhinandan@jewarintrnational.com>
2025-09-29 10:18:18 +0530

    changes

--- src/app/(admin)/faq/page.js
+++ src/app/(admin)/faq/page.js
@@ -9,6 +9,7 @@ export default function FAQ() {
   return (
     <div className="my-10 min-h-[80vh] p-4  mx-auto  bg-white rounded-md shadow ">
       <FaqTable/>
+      {/* hhtt */}
     </div>
   );
 }
