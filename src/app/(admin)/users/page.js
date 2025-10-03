7ebdf44da3c0db6df4bd9760551cc0a9f159e507
Abhinandan-2712 <abhinandan@jewarintrnational.com>
2025-10-03 18:27:58 +0530

    fix server issue

--- src/app/(admin)/users/page.js
+++ src/app/(admin)/users/page.js
@@ -1,8 +1,16 @@
-"use client";
-import dynamic from "next/dynamic";
+// "use client";
+// import dynamic from "next/dynamic";
 
-// Dynamically import User component with SSR disabled
-const User = dynamic(() => import("./User"), { ssr: false });
+// // Dynamically import User component with SSR disabled
+// const User = dynamic(() => import("./User"), { ssr: false });
+
+// export default function Page() {
+//   return <User />;
+// }
+// src/app/(admin)/users/page.js
+"use client"; // make this page a client component
+
+import User from "./User";
 
 export default function Page() {
   return <User />;
